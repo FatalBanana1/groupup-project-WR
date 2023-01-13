46121d8a064c793cfe7e484000d4d98b3c0502fa
fatalbanana <dark1knite@gmail.com>
2023-01-13 02:32:04 -0500

    nested-buttons

--- frontend/src/components/Navigation/ProfileButton.js
+++ frontend/src/components/Navigation/ProfileButton.js
@@ -1,54 +1,78 @@
 // frontend/src/components/Navigation/ProfileButton.js
 import React, { useState, useEffect, useRef } from "react";
-import { useDispatch } from 'react-redux';
-import * as sessionActions from '../../store/session';
+import { useDispatch } from "react-redux";
+import * as sessionActions from "../../store/session";
+import OpenModalButton from "../OpenModalButton";
+import LoginFormModal from "../LoginFormModal";
+import SignupFormModal from "../SignupFormModal";
 
 function ProfileButton({ user }) {
-  const dispatch = useDispatch();
-  const [showMenu, setShowMenu] = useState(false);
-  const ulRef = useRef();
+	const dispatch = useDispatch();
+	const [showMenu, setShowMenu] = useState(false);
+	const ulRef = useRef();
 
-  const openMenu = () => {
-    if (showMenu) return;
-    setShowMenu(true);
-  };
+	const openMenu = () => {
+		if (showMenu) return;
+		setShowMenu(true);
+	};
 
-  useEffect(() => {
-    if (!showMenu) return;
+	useEffect(() => {
+		if (!showMenu) return;
 
-    const closeMenu = (e) => {
-      if (!ulRef.current.contains(e.target)) {
-        setShowMenu(false);
-      }
-    };
+		const closeMenu = (e) => {
+			if (!ulRef.current.contains(e.target)) {
+				setShowMenu(false);
+			}
+		};
 
-    document.addEventListener('click', closeMenu);
+		document.addEventListener("click", closeMenu);
 
-    return () => document.removeEventListener("click", closeMenu);
-  }, [showMenu]);
+		return () => document.removeEventListener("click", closeMenu);
+	}, [showMenu]);
 
-  const logout = (e) => {
-    e.preventDefault();
-    dispatch(sessionActions.logout());
-  };
+	const logout = (e) => {
+		e.preventDefault();
+		dispatch(sessionActions.logout());
+	};
 
-  const ulClassName = "profile-dropdown" + (showMenu ? "" : " hidden");
+	const ulClassName = "profile-dropdown" + (showMenu ? "" : " hidden");
 
-  return (
-    <>
-      <button onClick={openMenu}>
-        <i className="fas fa-user-circle" />
-      </button>
-      <ul className={ulClassName} ref={ulRef}>
-        <li>{user.username}</li>
-        <li>{user.firstName} {user.lastName}</li>
-        <li>{user.email}</li>
-        <li>
-          <button onClick={logout}>Log Out</button>
-        </li>
-      </ul>
-    </>
-  );
+	return (
+		<>
+			<button onClick={openMenu}>
+				<i className="fas fa-user-circle" />
+			</button>
+			<ul className={ulClassName} ref={ulRef}>
+				{user ? (
+					<>
+						<li>{user.username}</li>
+						<li>
+							{user.firstName} {user.lastName}
+						</li>
+						<li>{user.email}</li>
+						<li>
+							<button onClick={logout}>Log Out</button>
+						</li>
+					</>
+				) : (
+					<>
+						<li>
+							<OpenModalButton
+								buttonText="Log In"
+								modalComponent={<LoginFormModal />}
+							/>
+						</li>
+						<li>
+							<OpenModalButton
+								buttonText="Sign Up"
+								modalComponent={<SignupFormModal />}
+							/>
+						</li>
+					</>
+				)}
+			</ul>
+		</>
+	);
 }
 
 export default ProfileButton;
